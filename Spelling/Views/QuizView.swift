5d09d5bacf2b0fb8bd9b578eb025997d0da84daa
Ben Lavallée <blavallee@lcs.on.ca>
2025-03-28 14:33:31 -0400

    Added stored input by user, added text field so that user can input their guess and added button and check mechanism to verify if guess is correct

--- Spelling/Views/QuizView.swift
+++ Spelling/Views/QuizView.swift
@@ -10,7 +10,13 @@ import SwiftUI
 struct QuizView: View {
     
     // MARK: Stored properties
+    
+    // Random food user needs to guess
     @State var currentItem = itemsToSpell.randomElement()!
+    
+    // Guess text inputed by the user
+    @State var guess: String = ""
+    
     
     // MARK: Computed properties
     var body: some View {
@@ -19,8 +25,28 @@ struct QuizView: View {
             Image(currentItem.imageName)
                 .resizable()
                 .scaledToFit()
+            
+            TextField("Guess the food", text: $guess)
+                .padding()
+                .frame(width: 300, height: 50)
+            
+            Button {
+                checkGuess()
+            } label : {
+                Text("Submit")
+            }
+            
         }
         
+    }
+    
+    //MARK: Functions
+    func checkGuess() {
+        if guess == currentItem.word {
+            print("correct")
+        } else {
+            print("incorrect")
+        }
     }
 }
 
